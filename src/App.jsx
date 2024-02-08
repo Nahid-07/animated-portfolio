3e6ad49739788508c30c5b7f06b7ed59233e17bf
Nahid-07 <nahidasif1998@gmail.com>
2024-02-08 22:15:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return <div>
+
+    <section>Hero</section>
+    <section>paralax</section>
+    <section>services</section>
+    <section>paralax</section>
+    <section>portfolio1</section>
+    <section>portfolio2</section>
+    <section>portfolio3</section>
+    <section>contact</section>
+  </div>;
+};
+
+export default App;
