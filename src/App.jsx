60edf98d185f41dcbc6d42ef4dc62765b2d5b3ab
Nahid-07 <nahidasif1998@gmail.com>
2024-02-10 00:15:41 +0600

    added links animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import Navbar from './component/navbar/Navbar';
 const App = () => {
   return <div>
 
-    <section>
-      <Navbar/>
+    <section id='Homepage'>
+    <Navbar/>
     </section>
-    <section>paralax</section>
-    <section>services</section>
-    <section>paralax</section>
-    <section>portfolio1</section>
-    <section>portfolio2</section>
+    <section id='Services'>services</section>
+    <section id='Portfolio'>paralax</section>
+    <section id='Contact'>portfolio1</section>
+    <section id='About'>portfolio2</section>
     <section>portfolio3</section>
     <section>contact</section>
   </div>;
