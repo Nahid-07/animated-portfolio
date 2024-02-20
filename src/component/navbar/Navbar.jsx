88d9476dbf0d2ee659dba0dbb72f41b80a7601b0
Nahid-07 <nahidasif1998@gmail.com>
2024-02-20 21:01:32 +0600

    added social links

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Sidebar/>
+      <Sidebar />
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
@@ -20,9 +20,30 @@ const Navbar = () => {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <img src="/facebook.png" alt="" />
-          <img src="instagram.png" alt="" />
-          <img src="public/youtube.png" alt="" />
+          <a
+            href="https://www.facebook.com/nahidul.islam.106902/"
+            target="_blank"
+          >
+            <img src="/facebook.png" alt="facebook" />
+          </a>
+          <a href="https://www.instagram.com/mr.asif_07_08/" target="_blank">
+            <img src="instagram.png" alt="" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nahidul-asif-islam/"
+            target="_blank"
+          >
+            <img src="public/linkedin.png" alt="" />
+          </a>
+          <a
+            href="https://www.fiverr.com/nahidul1998?up_rollout=true"
+            target="_blank"
+          >
+            <img src="public/fiverr-logo.png" alt="fiverr" />
+          </a>
+          <a href="https://github.com/Nahid-07" target="_blank">
+            <img src="public/gitHub.png" alt="" />
+          </a>
         </motion.div>
       </div>
     </nav>
