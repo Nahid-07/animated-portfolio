f523e9375b73aa7fdbeaf3b80654c8762b8b4d6a
Nahid-07 <nahidasif1998@gmail.com>
2024-03-07 15:10:45 +0600

    responsive navbar for mobile and tablet

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import './app.scss'
-import AboutMe from './component/aboutme/AboutMe';
-import Contact from './component/contact/Contact';
-import Hero from './component/hero/Hero';
-import MySkills from './component/mySkills/MySkills';
-import Navbar from './component/navbar/Navbar';
-import Portfolio from './component/portfolio/Portfolio';
+import "./app.scss";
+import AboutMe from "./component/aboutme/AboutMe";
+import Contact from "./component/contact/Contact";
+import Hero from "./component/hero/Hero";
+import MySkills from "./component/mySkills/MySkills";
+import Navbar from "./component/navbar/Navbar";
+import Portfolio from "./component/portfolio/Portfolio";
 const App = () => {
-  return <div>
-
-    <section id='Homepage'>
-    <Navbar/>
-    <Hero/>
-    </section>
-    {/* <section id='Services'><Parallax/></section> */}
-    <section id='About'>
-      <AboutMe/>
-    </section>
-    {/* <section id='Contact'><Parallax type="portfolio"/></section> */}
-    <section id='Skills'><MySkills/></section>
-    <Portfolio/>
-    <section id='Contact'><Contact/></section>
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        {/* <Hero /> */}
+      </section>
+      {/* <section id='Services'><Parallax/></section> */}
+      <section id="About">
+        {/* <AboutMe /> */}
+      </section>
+      {/* <section id='Contact'><Parallax type="portfolio"/></section> */}
+      <section id="Skills">
+        {/* <MySkills /> */}
+      </section>
+      {/* <Portfolio /> */}
+      <section id="Contact">
+        {/* <Contact /> */}
+      </section>
+    </div>
+  );
 };
 
 export default App;
